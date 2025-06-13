585cfa8af6aa6e20ed7802b0cf4915f3406d5355
Dan Fred <cablascablas@gmail.com>
2025-06-13 23:44:31 +0800

    AddLivewireclassServeSaleReleasing

---
+++ app/Livewire/ServeSaleReleasing.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ServeSaleReleasing extends Component
+{
+    public function render()
+    {
+        return view('livewire.serve-sale-releasing');
+    }
+}
